437a473e762ad40efd6c0790e53d06414fe10813
IvanMees <mees.ivan@hotmail.com>
2022-08-29 16:54:51 -0300

    ADD TRANSFORMATION

--- src/Transformation.java
+++ src/Transformation.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.security.cert.CertPath;
 
 public class Transformation {
 
@@ -32,8 +33,10 @@ public class Transformation {
 
     public BufferedImage applyProcess(BufferedImage img, double[][] mat, int x, int y) {
 
-        double newX = Math.round(x * mat[0][0] + y * mat[0][1] + mat[0][2]);
-        double newY = Math.round(x * mat[1][0] + y * mat[1][1] + mat[1][2]);
+
+        double newX = Math.round(x * mat[0][0] + x * mat[0][1] + mat[0][2]);
+        double newY = Math.round(y * mat[1][0] + y * mat[1][1] + mat[1][2]);
+
 
         if (newX < Constants.alteredImage.getWidth() && newY < Constants.alteredImage.getHeight() && newX >= 0 && newY >= 0) {
           img.setRGB((int) newX, (int) newY, Constants.alteredImage.getRGB(x, y));
@@ -44,7 +47,42 @@ public class Transformation {
 
     ActionListener enlargement = e ->{
 
+        double enlargement = 0.5;
+
+
+        BufferedImage translate = new BufferedImage(Constants.alteredImage.getWidth(), Constants.alteredImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+        BufferedImage img =  new BufferedImage(Constants.alteredImage.getWidth(), Constants.alteredImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        double[][] matrizEnlargement = {  { enlargement, 0, 0 },
+                                        { 0, enlargement, 0 },
+                                        { 0, 0, 1 } };
+
+
+        for (int j = 0; j < Constants.alteredImage.getHeight(); ++j) {
+            for (int i = 0; i < Constants.alteredImage.getWidth(); ++i) {
+                translate = applyProcess2(img, matrizEnlargement, i,j,enlargement);
+            }
+        }
+
+        Graphics2D g = (Graphics2D) Constants.myPanelImg.getGraphics();
+
+        g.drawImage(translate, 280, 150, 250, 150, null);
     };
+
+    public BufferedImage applyProcess2(BufferedImage img, double[][] mat, int x, int y,double enlarg) {
+
+
+
+        double out_x = Math.round(x * mat[0][0] + x * mat[0][1] + 1.0 * mat[0][2]);
+        double out_y = Math.round(y * mat[1][0] + y * mat[1][1] + 1.0 * mat[1][2]);
+
+
+        if (out_x < Constants.defaultImg.getWidth() && out_y < Constants.defaultImg.getHeight() && out_x >= 0 && out_y >= 0) {
+            img.setRGB((int) out_x, (int) out_y , Constants.alteredImage.getRGB(x, y));
+        }
+
+        return img;
+    }
 
     ActionListener reduction = e ->{
 
